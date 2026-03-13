bb7ee191f9f8e68474b97fcd0f783eeb52e29487
APOORVA YR - 220953033 - MITMPL <apoorva.mitmpl2022@learner.manipal.edu>
2026-03-13 10:34:46 +0530

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,73 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.5.2] - 2026-02-13
+### Changed
+- Use Python 3.14
+- Update dependencies
+
+### Internal
+- Add `uv` lockfile maintenance GitHub Workflow
+- Use native Vercel support for `uv`
+- Relax `uv` Python version patch number to workaround Vercel shenanigans
+
+## [0.5.1] - 2025-11-06
+### Fixed
+- Typo for cellphone field display
+- Display errors and field values with GET to home form
+
+## [0.5.0] - 2025-11-06
+### Added
+- Proper form fields data validation
+- Cellphone field (only available for vCard format) (thanks @zerabba)
+
+### Changed
+- Use Python 3.13.9
+- Update dependencies
+- Modernize Python type hints
+
+## [0.4.0] - 2025-09-29
+### Fixed
+- Code coverage reporting without using Codecov service
+
+### Changed
+- Use Python 3.13.5
+- Update dependencies
+- Replace usage of `safety` with `pip-audit`
+- Migrate to `pydantic` version 2
+- Use `uv` package manager instead of `pipenv`
+- Use `ruff` instead of `black` and `flake8`
+
+## [0.3.0] - 2023-08-03
+### Fixed
+- Generate QR codes with Unicode (UTF-8) encoding
+
+### Changed
+- Vendor external CSS stylesheet
+- Use Python 3.11.4
+- Update dependencies
+
+## [0.2.0] - 2022-08-30
+### Changed
+- Use Python 3.10.6
+- Update dependencies
+
+## [0.1.0] - 2022-03-20
+### Added
+- Initial release of `business-card-generator`
+- Support for QR-Code vCard and MeCard
+- Export format support for VCF, SVG, PNG
+
+[Unreleased]: https://github.com/rclement/business-card-generator/compare/0.5.2...HEAD
+[0.5.2]: https://github.com/rclement/business-card-generator/compare/0.5.1...0.5.2
+[0.5.1]: https://github.com/rclement/business-card-generator/compare/0.5.0...0.5.1
+[0.5.0]: https://github.com/rclement/business-card-generator/compare/0.4.0...0.5.0
+[0.4.0]: https://github.com/rclement/business-card-generator/compare/0.3.0...0.4.0
+[0.3.0]: https://github.com/rclement/business-card-generator/compare/0.2.0...0.3.0
+[0.2.0]: https://github.com/rclement/business-card-generator/compare/0.1.0...0.2.0
+[0.1.0]: https://github.com/rclement/business-card-generator/releases/tag/0.1.0
---
+++ business_card_generator/app.py
@@ -0,0 +1,210 @@
+import mimetypes
+
+from http import HTTPStatus
+from flask import (
+    Blueprint,
+    Flask,
+    abort,
+    redirect,
+    render_template,
+    request,
+    send_file,
+    url_for,
+)
+from pydantic import ValidationError
+from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.wrappers import Response
+from whitenoise import WhiteNoise
+
+from . import __about__
+from .card import CardParams, MeCard, VCard
+from .settings import Settings
+
+
+# ------------------------------------------------------------------------------
+
+
+views_bp = Blueprint("views", __name__)
+
+
+def validate_card_params(
+    data: dict[str, str],
+) -> tuple[CardParams | None, dict[str, str] | None]:
+    try:
+        card_params = CardParams(**data)
+        field_errors = None
+    except ValidationError as exc:
+        card_params = None
+        field_errors = {
+            str(error["loc"][0]) if error["loc"] else "unknown": error["msg"]
+            for error in exc.errors()
+        }
+    return card_params, field_errors
+
+
+@views_bp.get("/")
+def get_home() -> str:
+    input_data = request.args.to_dict()
+    card_type = input_data.get("card_type")
+    if input_data:
+        card_params, field_errors = validate_card_params(input_data)
+    else:
+        card_params = None
+        field_errors = None
+
+    return render_template(
+        "home.html",
+        card_type=card_type,
+        card_params=card_params.model_dump(exclude_none=True)
+        if card_params
+        else input_data,
+        field_errors=field_errors,
+    )
+
+
+@views_bp.post("/")
+def post_home() -> Response | tuple[str, HTTPStatus]:
+    input_data = request.form.to_dict()
+    card_type = input_data.get("card_type", "vcard")
+    card_params, field_errors = validate_card_params(input_data)
+
+    if not card_params or field_errors:
+        return render_template(
+            "home.html", card_params=input_data, field_errors=field_errors
+        ), HTTPStatus.UNPROCESSABLE_ENTITY
+
+    return redirect(
+        url_for(
+            "views.get_card",
+            card_type=card_type,
+            **card_params.model_dump(exclude_none=True),
+        )
+    )
+
+
+@views_bp.get("/card")
+def get_card() -> str:
+    return render_template("card.html")
+
+
+@views_bp.get("/vcard.svg")
+def get_vcard_svg() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    vcard = VCard(card_params)
+    return send_file(
+        vcard.qrcode_svg(),
+        mimetype=mimetypes.types_map[".svg"],
+        download_name="vcard.svg",
+    )
+
+
+@views_bp.get("/vcard.png")
+def get_vcard_png() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    vcard = VCard(card_params)
+    return send_file(
+        vcard.qrcode_png(),
+        mimetype=mimetypes.types_map[".png"],
+        download_name="vcard.png",
+    )
+
+
+@views_bp.get("/vcard.vcf")
+def get_vcard_vcf() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    vcard = VCard(card_params)
+    return send_file(
+        vcard.vcf(),
+        mimetype=mimetypes.types_map[".vcf"],
+        download_name="vcard.vcf",
+    )
+
+
+@views_bp.get("/mecard.svg")
+def get_mecard_svg() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    mecard = MeCard(card_params)
+    return send_file(
+        mecard.qrcode_svg(),
+        mimetype=mimetypes.types_map[".svg"],
+        download_name="mecard.svg",
+    )
+
+
+@views_bp.get("/mecard.png")
+def get_mecard_png() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    mecard = MeCard(card_params)
+    return send_file(
+        mecard.qrcode_png(),
+        mimetype=mimetypes.types_map[".png"],
+        download_name="mecard.png",
+    )
+
+
+@views_bp.get("/mecard.vcf")
+def get_mecard_vcf() -> Response:
+    card_params, field_errors = validate_card_params(request.args.to_dict())
+    if not card_params or field_errors:
+        abort(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    mecard = MeCard(card_params)
+    return send_file(
+        mecard.vcf(),
+        mimetype=mimetypes.types_map[".vcf"],
+        download_name="mecard.vcf",
+    )
+
+
+# ------------------------------------------------------------------------------
+
+
+def create_app(env_file: str | None = ".env") -> Flask:
+    settings = Settings(_env_file=env_file)
+
+    app = Flask(__name__)
+    app.config.update(
+        about=dict(
+            name=__about__.__name__,
+            description=__about__.__description__,
+            version=__about__.__version__,
+        ),
+    )
+    app.debug = settings.app_environment == "development"
+    app.testing = settings.app_environment == "testing"
+
+    app.wsgi_app = WhiteNoise(  # type: ignore[method-assign]
+        app.wsgi_app,
+        root=app.static_folder,
+        prefix=app.static_url_path,
+        autorefresh=app.debug,
+    )
+    app.wsgi_app = ProxyFix(  # type: ignore[method-assign]
+        app.wsgi_app, x_proto=1, x_host=1
+    )
+
+    @app.before_request
+    def _force_https() -> Response | None:
+        if settings.force_https and request.url.startswith("http://"):
+            https_url = request.url.replace("http://", "https://", 1)
+            return redirect(https_url)
+        return None
+
+    app.register_blueprint(views_bp, url_prefix="")
+
+    return app
